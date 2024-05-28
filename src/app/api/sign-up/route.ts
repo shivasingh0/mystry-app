f21487f63bdc632d2cca6ca093ce5343540ef3a2
shivasingh0 <shiva2003rajawat@gmail.com>
2024-05-28 10:53:47 +0530

    setup for resend email and signup route created

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,106 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+
+    // Check existing user in database by username
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // Check existing user in database by email
+    const existingUserByEmail = await UserModel.findOne({
+      email,
+    });
+
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json({
+            success: false,
+            message: "User already exist with this email",
+        }, {status: 400} )
+      } else{
+        const hashedPassword = await bcrypt.hash(password, 10)
+        existingUserByEmail.password = hashedPassword,
+        existingUserByEmail.verifyCode = verifyCode,
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+        // save user
+        await existingUserByEmail.save()
+      }
+    } else {
+      // Create new user
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getDate() + 1);
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        isVerified: false,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isAcceptMessage: true,
+        messages: [],
+      });
+      await newUser.save(); // Save user to database
+    }
+
+    // send verification email
+    const emailResponse = await sendVerificationEmail(
+      email,
+      verifyCode,
+      username
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+        {
+          success: true,
+          message: "User registered successfully. Please verify your email",
+        },
+        { status: 201 }
+      );
+
+  } catch (error) {
+    console.error("Error registring user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error registring user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,55 +1,55 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface Message extends Document {
-    content: string;
-    createdAt: Date;
+  content: string;
+  createdAt: Date;
 }
 
 const MessageSchema: Schema<Message> = new Schema({
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, required: true },
 });
 
 export interface User extends Document {
-    username: string;
-    password: string;
-    email: string;
-    verifyCode: string;
-    verifyCodeExpiry: Date;
-    isVerified: boolean;
-    isAcceptMessage: boolean;
-    messages: Message[];
+  username: string;
+  password: string;
+  email: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptMessage: boolean;
+  messages: Message[];
 }
 
-const UserSchema: Schema<User> = new Schema(
-    {
-        username: {
-            type: String,
-            required: [true, "Username is required"],
-            unique: true,
-            trim: true,
-        },
-        password: { type: String, required: [true, "Password is required"] },
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-            unique: true,
-            match: [
-                /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                "Please use a valid email",
-            ],
-        },
-        verifyCode: { type: String, required: [true, "Verify code is required"] },
-        verifyCodeExpiry: {
-            type: Date,
-            required: [true, "Verify code expiry is required"],
-        },
-        isVerified: { type: Boolean, default: false },
-        isAcceptMessage: { type: Boolean, default: true },
-        messages: [MessageSchema],
-    }
-);
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true,
+  },
+  password: { type: String, required: [true, "Password is required"] },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+      "Please use a valid email",
+    ],
+  },
+  verifyCode: { type: String, required: [true, "Verify code is required"] },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+  isVerified: { type: Boolean, default: false },
+  isAcceptMessage: { type: Boolean, default: true },
+  messages: [MessageSchema],
+});
 
-const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User", UserSchema))
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
 
 export default UserModel;
