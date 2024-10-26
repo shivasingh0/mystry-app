5bb8965f25552a7a7a10419fee3fc50913a7f088
shivasingh0 <shiva2003rajawat@gmail.com>
2024-10-26 21:52:57 +0530

    "Update TypeScript to 5.6.3, modify dashboard page and user model, and update tsconfig.json"

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -66,7 +66,8 @@ const Dashboard = () => {
         const response = await axios.get<ApiResponse>("/api/get-messages");
         console.log(response.data.message)
 
-        setMessages((prevMessages) => [...prevMessages, ...response.data.message]);
+
+        setMessages(response.data.message as Message[]);
 
         if (refresh) {
           toast({
